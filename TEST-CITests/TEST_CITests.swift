4b085c26ae1de5628170b17d0bfbba637bd04c20
Kevin <kevin.bertrand@digimobee.com>
2023-11-20 11:57:26 +0100

    Fixing tests

--- TEST-CITests/TEST_CITests.swift
+++ TEST-CITests/TEST_CITests.swift
@@ -19,7 +19,7 @@ final class TEST_CITests: XCTestCase {
     }
 
     func testExample() throws {
-        XCTFail()
+//        XCTFail()
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
