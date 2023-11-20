1a1895e4838a0777614556b1c9ffbb782631d0ea
Kevin <kevin.bertrand@digimobee.com>
2023-11-20 11:15:53 +0100

    Initial Commit

---
+++ TEST-CI/TEST_CIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEST_CIApp.swift
+//  TEST-CI
+//
+//  Created by VM Test on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TEST_CIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
